b6f5951ff220d0bbd51cebd9d7fb61375e171dcc
James Spencer <thisjustosay@gmail.com>
2014-12-29 20:27:48 -0700

    removing example

--- example/app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.use(express.static(__dirname + '/www'));
-app.use(express.static(__dirname + '/bower_components'));
-app.listen(process.env.PORT || 9292);
-
